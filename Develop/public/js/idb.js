a44b6cf307e6830b2163ecaa4cfb826160ae5b22
Yunzhu Jin <jyz022020@gmail.com>
2022-06-16 20:12:56 -0700

    add IDB

---
+++ Develop/public/js/idb.js
@@ -0,0 +1,77 @@
+// create variable to hold db connection
+let db;
+// establish a connection to IndexedDB database called 'pizza_hunt' and set it to version 1
+const request = indexedDB.open('budget', 1);
+
+request.onupgradeneeded = function(event) {
+    // save a reference to the database 
+    const db = event.target.result;
+    // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+  };
+
+  // upon a successful 
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+    db = event.target.result;
+  
+    // check if app is online, if yes run uploadPizza() function to send all local db data to api
+    if (navigator.onLine) {
+      // we haven't created this yet, but we will soon, so let's comment it out for now
+      uploadTransaction();
+    }
+  };
+  
+  request.onerror = function(event) {
+    console.log(event.target.errorCode);
+  };
+
+  // This function will be executed if we attempt to submit a new transaction and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+  
+    // access the object store for `new_pizza`
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+  
+    // add record to your store with add method
+    transactionObjectStore.add(record);
+
+    alert("Your transaction submitted");
+  }
+
+  function uploadTransaction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+    const allRecords = transactionObjectStore.getAll();
+
+    allRecords.onsuccess = function() {
+        if (allRecords.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(allRecords.result),
+                headers: {
+                  Accept: "application/json, text/plain, */*",
+                  "Content-Type": "application/json"
+              }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    clearIDB();
+                    console.log("uploaded local data to server and clear up local data")
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    }
+}
+
+function clearIDB() {
+  const transaction = db.transaction(['new_transaction'], 'readwrite');
+  const transactionObjectStore = transaction.objectStore('new_transaction');
+  transactionObjectStore.clear();
+}
